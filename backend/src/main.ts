a85105947dde4acf268fb8e25a4f4c58a8b06156
Kirucoderanger <klemu@byupathway.edu>
2026-05-31 11:11:24 +0300

    Configure production CORS settings

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -47,7 +47,10 @@ async function bootstrap() {
   );
 
   app.enableCors({
-  origin: process.env.FRONTEND_URL,
+  origin: [
+    'http://localhost:3000',
+    'https://property-listing-platform-ten.vercel.app/',
+  ],
   credentials: true,
 });
 
